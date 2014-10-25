24aba4187c09d4341db42ada5e6506bb30bb3065
Jay Lorenzo <cxplanet@hotmail.com>
2014-10-24 17:30:34 -0700

    keyframe with bezier, defaults work

--- SleepyTime/SettingsController.swift
+++ SleepyTime/SettingsController.swift
@@ -12,11 +12,33 @@ import CoreLocation
 class SettingsController: UIViewController
 {
     @IBOutlet weak var screenBrightness: UISlider?
+    @IBOutlet weak var timePicker: UIDatePicker?
+    
+    let hoursKey = "alarmTimeHrs"
+    let minutesKey = "alarmTimeMins"
+    
+    var alarmHour : Int = 0
+    var alarmMin : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Settings"
         screenBrightness?.value = Float(UIScreen.mainScreen().brightness)
+        
+        // see if we have an awake time already set
+        timePicker?.addTarget(self, action: Selector("timePickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        updateAlarmPickerWithUserDefaults()
+    }
+    
+    func timePickerChanged(datePicker:UIDatePicker) {
+        var dateFormatter = NSDateFormatter()
+        
+        let calendar = NSCalendar.currentCalendar()
+        let dateParts = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: timePicker!.date)
+        
+        NSLog("Hour is \(dateParts.hour)  minutes \(dateParts.minute)")
+        
+        updateAlarmDefaults(dateParts.hour, mins: dateParts.minute)
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,9 +46,32 @@ class SettingsController: UIViewController
         // Dispose of any resources that can be recreated.
     }
     
+    func updateAlarmPickerWithUserDefaults()
+    {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if let hours = defaults.objectForKey(hoursKey) as? Int {
+            if let minutes = defaults.objectForKey(minutesKey) as? Int {
+                // set up the timer with a new date
+                NSLog("Defaults: Alarm hour is \(hours)  minutes \(minutes)")
+            }
+        }
+    }
+    
+    func updateAlarmDefaults(hours : Int, mins : Int)
+    {
+        let defaults = NSUserDefaults.standardUserDefaults()
+    
+        defaults.setInteger(hours, forKey: hoursKey)
+        defaults.setInteger(mins, forKey: minutesKey)
+        
+        NSLog("Set defaults: Alarm hour is \(hours)  minutes \(mins)")
+    }
+    
     @IBAction func changeBrightness(sender: UISlider)
     {
         NSLog("Current slider value: \(sender.value)")
+        
     }
     
 }
--- SleepyTime/ViewController.swift
+++ SleepyTime/ViewController.swift
@@ -181,6 +181,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPopoverPres
         }
     }
     
+    func shootingStar(starImg : UIImageView)
+    {
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 16,y: 239))
+        path.addCurveToPoint(CGPoint(x: 301, y: 239), controlPoint1: CGPoint(x: 136, y: 373), controlPoint2: CGPoint(x: 178, y: 110))
+        
+        // create a new CAKeyframeAnimation that animates the objects position
+        let anim = CAKeyframeAnimation(keyPath: "position")
+        
+        // set the animations path to our bezier curve
+        anim.path = path.CGPath
+        
+        // set some more parameters for the animation
+        // this rotation mode means that our object will rotate so that it's parallel to whatever point it is currently on the curve
+        anim.rotationMode = kCAAnimationRotateAuto
+        anim.repeatCount = Float.infinity
+        anim.duration = 5.0
+        
+        // we add the animation to the squares 'layer' property
+        starImg.layer.addAnimation(anim, forKey: "animate position along path")
+    }
+    
     // colorize function takes HEX and Alpha converts then returns aUIColor object
     func colorize (hex: Int, alpha: Double = 1.0) -> UIColor {
         let red = Double((hex & 0xFF0000) >> 16) / 255.0
@@ -214,7 +236,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPopoverPres
     {
         let alphaVal : CGFloat = showAll ? 1.0 : 0.0
         showSleepTime()
-        NSLog("Reset star count: \(self.minorStars?.count)")
+        // NSLog("Reset star count: \(self.minorStars?.count)")
         for minorStar in self.minorStars! {
             UIView.animateWithDuration(0.5, animations: {
                 minorStar.transform = CGAffineTransformIdentity
